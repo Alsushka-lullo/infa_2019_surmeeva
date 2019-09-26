763fec3737bfc3ac5f339f9c7b9863a6dec58200
Alsushka-lullo <Surmeeva.Alsu@gmail.com>
2019-09-26 12:01:06 +0300

    picture 16.2

--- lab3/lab_3_task_2.py
+++ lab3/lab_3_task_2.py
@@ -8,7 +8,7 @@ penSize(0)
 
 
 """
-def oval(x1, y1, x2, y2, color):
+def oval(x1, y1, x2, y2, color):  #don't forget to delete
     x_cen = (x1 + x2) / 2
     y_cen = (y1 + y2) / 2
     a = (x2 - x1) / 2 
@@ -23,19 +23,19 @@ def oval(x1, y1, x2, y2, color):
         lineTo(x_pos, y_pos)
 """
 
-def ellipse(x1, y1, x2, y2, color):
+def ellipse(x1, y1, x2, y2, color): #drawingColoredEllipses
     brushColor(color)
     _ellipse = circle(10, 10, 10)
     changeCoords(_ellipse, [(x1, y1), (x2, y2)])
     return _ellipse
 
 
-def rectangle_col(x1, y1, x2, y2, color):
+def rectangle_col(x1, y1, x2, y2, color): #drawingColoredRectangles
     brushColor(color)
     rectangle(x1, y1, x2, y2)
 
 
-rectangle_col(0, 0, 570, 525, "#C5C7C9")
+rectangle_col(0, 0, 570, 525, "#C5C7C9") #housesAndClouds
 rectangle_col(0, 535, 570, 830, "#5F726A")
 rectangle_col(13, 20, 127, 548, "#90A6A7")
 rectangle_col(150, 46, 268, 559, "#82A68E")
@@ -51,7 +51,7 @@ ellipse(-78, 600, 80, 645, "#BCC4BA")
 ellipse(37, 657, 197, 702, "#BCC4BA")
 ellipse(41, 710, 198, 758, "#BCC4BA")
 
-ellipse(209, 744, 229, 752, "black")
+ellipse(209, 744, 229, 752, "black") #drawingCar
 rectangle_col(223, 715, 462, 758, "#65C2DD")
 rectangle_col(267, 690, 384, 717, "#65C2DD")
 rectangle_col(275, 697, 313, 715, "#BCE0F0")
