953fa954fd446f735c61fc9cf9020d6746d7a1f9
Alsu <surmeeva.alsu@gmail.com>
2019-10-24 05:16:45 +0300

    oh, a lot of errors, but almost working

--- Lab6/goals.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-list_of_names = []
-for i in range(0, len(list_of_names), 2):
-    list_of_names[i].grid(row=i, column=1)
-    list_of_names[i+1].grid(row=i, column=2)
-
-mainloop()
